fa7449c8967ad94a8f6e296110063b556cfb3bf5
Sebastien Gaumin <sgaumin@gmail.com>
2018-10-16 18:42:06 +0200

    2D Unity Project + Player Prefab

---
+++ Assets/Scripts/PlayerCommands.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCommands : MonoBehaviour {
+
+    public GameObject paper;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Building"))
+        {
+            Debug.Log("Building");
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Instantiate(paper, transform.position + transform.up, Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        Vector3 inputs;
+
+        inputs = new Vector3(Input.GetAxisRaw("Horizontal"), 0);
+
+        rb.AddForce(inputs * speed);
+	}
+}
