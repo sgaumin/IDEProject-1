3ad20fa397caab77e685f46e2d0b21698a0def1c
Sebastien Gaumin <sgaumin@gmail.com>
2018-10-16 15:42:55 +0200

    First Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GameJam {
+    public class GameManager : MonoBehaviour {
+
+	    public static GameManager instance = null;
+
+	    public enum gameStates {Playing, End};
+	    public gameStates gameState = gameStates.Playing;
+
+	    void Awake(){
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+	    public void LoadScene(string scene){
+		    SceneManager.LoadScene (scene);
+	    }
+
+	    public void ReloagScene(){
+		    SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+	    }
+
+        public void QuitGame() {
+            Application.Quit();
+        }
+
+    }
+}
