74eb36b57b4868a684152d761b72a46d003dcef1
Sebastien Gaumin <sgaumin@gmail.com>
2018-10-17 17:01:28 +0200

    Cinematique Script

---
+++ Assets/Scripts/CinematiqueTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CinematiqueTrigger : MonoBehaviour {
+
+    public Cinematique cine;
+    public Image fond;
+    
+    public void StartCine()
+    {
+        StartCoroutine(Display());
+    }
+	
+	IEnumerator Display()
+    {
+        foreach (Sprite sprite in cine.sprites)
+        {
+            fond.sprite = sprite;
+            yield return new WaitForSeconds(cine.timeBetweenSprite);
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/Cinematique.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Cinematique")]
+public class Cinematique : ScriptableObject {
+    public Sprite[] sprites;
+    public float timeBetweenSprite;
+}
