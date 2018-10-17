8aa8a8a63fed6fb8d2203a8166b4a6df846100a9
nadege-bourguignon <nadegepub@gmail.com>
2018-10-17 11:49:43 +0200

    Ajout du jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,41 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Boo.Lang;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
-    [Header("Player Settings")]
-    public float moveSpeed;
+    [Header("Player Settings")] public float moveSpeed;
     public float jumpForce;
 
     private Rigidbody2D rb;
     private bool grounded;
     private bool facingRight = true;
+    private Collider2D collider;
 
     // Use this for initialization
-    void Start() {
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
         grounded = true;
     }
 
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
 
         // Horizontal movements
         Vector3 inputs = new Vector3(Input.GetAxisRaw("Horizontal"), 0);
-        if (Input.GetAxisRaw("Horizontal") > 0 && !facingRight){
+        if (Input.GetAxisRaw("Horizontal") > 0 && !facingRight)
+        {
             Flip();
         }
-        else if(Input.GetAxisRaw("Horizontal") < 0 && facingRight){
+        else if (Input.GetAxisRaw("Horizontal") < 0 && facingRight)
+        {
             Flip();
         }
         rb.AddForce(inputs * moveSpeed);
-
-        // Jump (a revoir)
-        //if (Input.GetAxisRaw("Vertical") > 0 && grounded)
-        //{
-        //    rb.AddForce(transform.up * jumpForce);
-        //    grounded = false;
-        //}
+        Debug.Log(grounded);
+        if (Input.GetKeyDown(KeyCode.Space)&&grounded) //todo attendre de toucher le sol
+        {
+            Jump();
+        }
+        
     }
 
     // Pivot the player according to his direction
@@ -47,4 +53,20 @@ public class PlayerMovement : MonoBehaviour {
         theScale.x *= -1;
         transform.localScale = theScale;
     }
+
+    void Jump()
+    {
+        rb.AddForce(new Vector3(0, jumpForce, 0));
+        grounded = false;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (!grounded)
+        {
+            grounded = true;
+        }
+        
+    }
+
 }
